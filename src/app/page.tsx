26faa96f7c7f3acbe52acfb7f68a4adb1e57ef8a
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-10-13 15:57:28 +0200

    display articles from server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { cookies } from 'next/headers';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+
 export default async function Home() {
-  return <div>Soon...</div>;
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const { data: articles = [] } = await supabase.from('article').select();
+  return (
+    <div>
+      {articles?.map((article) => (
+        <span key={article.id}>{article.title}</span>
+      ))}
+    </div>
+  );
 }
