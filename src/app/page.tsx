00da79496adffc866ecfb6dd98b8e07b3ce3a39e
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-10-13 12:35:37 +0200

    fix ui css path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { cookies } from 'next/headers';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-
 export default async function Home() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
-  const { data: articles } = await supabase.from('article').select();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test1
-    </main>
-  );
+  return <div>Soon...</div>;
 }
