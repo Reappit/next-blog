38b0325fbb99b07b20391e4b629739f41f85be28
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-10-15 10:25:50 +0200

    enabled edge runtime for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
+export const runtime = 'edge'
+
 export default async function Home() {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({ cookies: () => cookieStore });
