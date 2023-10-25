ec0ff2f55285b5fa7ae80cde59a112aa9932b534
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-10-25 10:40:37 +0200

    use mdx remote, delete unused dependencies

--- src/app/(article-full-story)/[category]/[slug]/page.tsx
+++ src/app/(article-full-story)/[category]/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { UserCircle } from 'lucide-react';
 import PublishedDate from '@/components/PublisedDate';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { Suspense } from 'react';
 
 export default async function ArticlePage({
   params: { slug },
@@ -20,27 +22,31 @@ export default async function ArticlePage({
     .limit(1);
   const article = data?.at(0) as Article;
   return (
-    <article>
-      <div className="flex justify-center">
-        <section className="max-w-[680px]">
-          <div className="mt-[1.19em]">
-            <h1 className="text-4xl font-bold">{article.title}</h1>
-          </div>
-          <div className="mt-8 flex items-center">
-            <div>
-              <UserCircle size={50} strokeWidth={0.5} />
+    <Suspense fallback={<>Loading...</>}>
+      <article>
+        <div className="flex justify-center">
+          <section className="max-w-[680px]">
+            <div className="mt-[1.19em]">
+              <h1 className="text-4xl font-bold">{article.title}</h1>
             </div>
-            <div className="ml-3">
-              <div>Admin</div>
+            <div className="mt-8 flex items-center">
               <div>
-                <PublishedDate date={article.created_at} />
+                <UserCircle size={50} strokeWidth={0.5} />
+              </div>
+              <div className="ml-3">
+                <div>Admin</div>
+                <div>
+                  <PublishedDate date={article.created_at} />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="mt-10 border-y-[1px] px-2 py-[3px]">qqq</div>
-          <div className="mt-10">{article.full_story}</div>
-        </section>
-      </div>
-    </article>
+            <div className="mt-10 border-y-[1px] px-2 py-[3px]">qqq</div>
+            <div className="mt-10">
+              <MDXRemote source={article.full_story ?? ''} />
+            </div>
+          </section>
+        </div>
+      </article>
+    </Suspense>
   );
 }
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -7,27 +7,170 @@ import {
   listsPlugin,
   quotePlugin,
   thematicBreakPlugin,
+  toolbarPlugin,
+  linkDialogPlugin,
+  linkPlugin,
+  codeBlockPlugin,
+  sandpackPlugin,
+  codeMirrorPlugin,
+  type SandpackConfig,
+  KitchenSinkToolbar,
+  type DirectiveDescriptor,
+  imagePlugin,
+  tablePlugin,
+  frontmatterPlugin,
+  directivesPlugin,
+  AdmonitionDirectiveDescriptor,
+  diffSourcePlugin,
+  markdownShortcutPlugin,
 } from '@mdxeditor/editor';
 import { type MutableRefObject } from 'react';
+
+import { type LeafDirective } from 'mdast-util-directive';
 
 interface EditorProps {
   markdown: string;
   editorRef?: MutableRefObject<MDXEditorMethods | null>;
 }
 
+const defaultSnippetContent = `
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`.trim();
+
+interface YoutubeDirectiveNode extends LeafDirective {
+  name: 'youtube';
+  attributes: { id: string };
+}
+
+const YoutubeDirectiveDescriptor: DirectiveDescriptor<YoutubeDirectiveNode> = {
+  name: 'youtube',
+  type: 'leafDirective',
+  testNode(node) {
+    return node.name === 'youtube';
+  },
+  attributes: ['id'],
+  hasChildren: false,
+  Editor: ({ mdastNode, lexicalNode, parentEditor }) => {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}
+      >
+        <button
+          onClick={() => {
+            parentEditor.update(() => {
+              lexicalNode.selectNext();
+              lexicalNode.remove();
+            });
+          }}
+        >
+          delete
+        </button>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${mdastNode.attributes?.id}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </div>
+    );
+  },
+};
+
+export const virtuosoSampleSandpackConfig: SandpackConfig = {
+  defaultPreset: 'react',
+  presets: [
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live react',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'React',
+      name: 'react',
+      meta: 'live',
+      sandpackTemplate: 'react',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.js',
+      snippetLanguage: 'jsx',
+      initialSnippetContent: defaultSnippetContent,
+    },
+    {
+      label: 'Virtuoso',
+      name: 'virtuoso',
+      meta: 'live virtuoso',
+      sandpackTemplate: 'react-ts',
+      sandpackTheme: 'light',
+      snippetFileName: '/App.tsx',
+      initialSnippetContent: defaultSnippetContent,
+      dependencies: {
+        'react-virtuoso': 'latest',
+        '@ngneat/falso': 'latest',
+      },
+      // files: {
+      //   '/data.ts': dataCode,
+      // },
+    },
+  ],
+};
+
+export const ALL_PLUGINS = [
+  toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
+  listsPlugin(),
+  quotePlugin(),
+  headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
+  linkPlugin(),
+  linkDialogPlugin(),
+  imagePlugin({
+    imageAutocompleteSuggestions: [
+      'https://via.placeholder.com/150',
+      'https://via.placeholder.com/150',
+    ],
+  }),
+  tablePlugin(),
+  thematicBreakPlugin(),
+  frontmatterPlugin(),
+  codeBlockPlugin({ defaultCodeBlockLanguage: 'txt' }),
+  sandpackPlugin({ sandpackConfig: virtuosoSampleSandpackConfig }),
+  codeMirrorPlugin({
+    codeBlockLanguages: {
+      js: 'JavaScript',
+      css: 'CSS',
+      txt: 'text',
+      tsx: 'TypeScript',
+    },
+  }),
+  directivesPlugin({
+    directiveDescriptors: [
+      YoutubeDirectiveDescriptor,
+      AdmonitionDirectiveDescriptor,
+    ],
+  }),
+  diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
+  markdownShortcutPlugin(),
+];
+
 export default function Editor({ markdown, editorRef }: EditorProps) {
   return (
-    <div>
-      <MDXEditor
-        ref={editorRef}
-        markdown={markdown}
-        plugins={[
-          headingsPlugin(),
-          listsPlugin(),
-          quotePlugin(),
-          thematicBreakPlugin(),
-        ]}
-      />
+    <div className="mt-10">
+      <MDXEditor ref={editorRef} markdown={markdown} plugins={ALL_PLUGINS} />
     </div>
   );
 }
