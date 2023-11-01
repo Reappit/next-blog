baad6493feabc5fe6819bbf786744ed47be3d841
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-11-01 18:47:42 +0100

    fix eslint errors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,6 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  return twMerge(clsx(inputs)) as string;
 }
