319af1cb01811f45b4e0080880ee300ccbc3f086
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-11-06 00:34:56 +0100

    use editor toolbar

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -11,21 +11,18 @@ import {
   linkDialogPlugin,
   linkPlugin,
   codeBlockPlugin,
-  sandpackPlugin,
   codeMirrorPlugin,
-  type SandpackConfig,
   imagePlugin,
   tablePlugin,
   frontmatterPlugin,
   directivesPlugin,
   AdmonitionDirectiveDescriptor,
-  diffSourcePlugin,
   markdownShortcutPlugin,
-  KitchenSinkToolbar,
 } from '@mdxeditor/editor';
 import React, { type MutableRefObject, useEffect, useRef } from 'react';
 import { Textarea } from '@/components/ui/textarea';
 import autosize from 'autosize';
+import EditorToolbar from '@/components/EditorToolbar';
 
 interface EditorProps {
   subtitle: string;
@@ -34,61 +31,14 @@ interface EditorProps {
   editorRef?: MutableRefObject<MDXEditorMethods | null>;
 }
 
-const defaultSnippetContent = `
-  export default function App() {
-    return (
-      <div>
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-      </div>
-    );
-  }
-`.trim();
-
-export const virtuosoSampleSandpackConfig: SandpackConfig = {
-  defaultPreset: 'react',
-  presets: [
-    {
-      label: 'React',
-      name: 'react',
-      meta: 'live react',
-      sandpackTemplate: 'react',
-      sandpackTheme: 'light',
-      snippetFileName: '/App.js',
-      snippetLanguage: 'jsx',
-      initialSnippetContent: defaultSnippetContent,
-    },
-    {
-      label: 'React',
-      name: 'react',
-      meta: 'live',
-      sandpackTemplate: 'react',
-      sandpackTheme: 'light',
-      snippetFileName: '/App.js',
-      snippetLanguage: 'jsx',
-      initialSnippetContent: defaultSnippetContent,
-    },
-    {
-      label: 'Virtuoso',
-      name: 'virtuoso',
-      meta: 'live virtuoso',
-      sandpackTemplate: 'react-ts',
-      sandpackTheme: 'light',
-      snippetFileName: '/App.tsx',
-      initialSnippetContent: defaultSnippetContent,
-      dependencies: {
-        'react-virtuoso': 'latest',
-        '@ngneat/falso': 'latest',
-      },
-    },
-  ],
-};
-
-export const ALL_PLUGINS = [
-  toolbarPlugin({ toolbarContents: () => <KitchenSinkToolbar /> }),
-  listsPlugin(),
-  quotePlugin(),
+const editorPlugins = [
+  toolbarPlugin({
+    toolbarContents: () => <EditorToolbar />,
+  }),
   headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
+  quotePlugin(),
+  listsPlugin(),
+  thematicBreakPlugin(),
   linkPlugin(),
   linkDialogPlugin(),
   imagePlugin({
@@ -98,10 +48,7 @@ export const ALL_PLUGINS = [
     ],
   }),
   tablePlugin(),
-  thematicBreakPlugin(),
-  frontmatterPlugin(),
   codeBlockPlugin({ defaultCodeBlockLanguage: 'ts' }),
-  sandpackPlugin({ sandpackConfig: virtuosoSampleSandpackConfig }),
   codeMirrorPlugin({
     codeBlockLanguages: {
       ts: 'TypeScript',
@@ -113,8 +60,8 @@ export const ALL_PLUGINS = [
   directivesPlugin({
     directiveDescriptors: [AdmonitionDirectiveDescriptor],
   }),
-  diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' }),
   markdownShortcutPlugin(),
+  frontmatterPlugin(),
 ];
 
 export default function Editor({
@@ -151,7 +98,7 @@ export default function Editor({
         <MDXEditor
           ref={editorRef}
           markdown={fullStory}
-          plugins={ALL_PLUGINS}
+          plugins={editorPlugins}
           contentEditableClassName="prose"
           className="editor-root mt-[1.19em] w-full"
           onChange={(val) => {
---
+++ src/components/EditorToolbar.tsx
@@ -0,0 +1,97 @@
+import {
+  type AdmonitionKind,
+  BlockTypeSelect,
+  BoldItalicUnderlineToggles,
+  Button,
+  ChangeAdmonitionType,
+  ChangeCodeMirrorLanguage,
+  CodeToggle,
+  ConditionalContents,
+  CreateLink,
+  type EditorInFocus,
+  InsertAdmonition,
+  InsertCodeBlock,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  ListsToggle,
+  Separator,
+  ShowSandpackInfo,
+} from '@mdxeditor/editor';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { type DirectiveNode } from '@mdxeditor/editor/dist/plugins/directives/DirectiveNode';
+import { SaveIcon } from 'lucide-react';
+
+function whenInAdmonition(editorInFocus: EditorInFocus | null) {
+  const node = editorInFocus?.rootNode;
+  if (!node || node.getType() !== 'directive') {
+    return false;
+  }
+
+  return ['note', 'tip', 'danger', 'info', 'caution'].includes(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
+    (node as DirectiveNode).getMdastNode().name as AdmonitionKind,
+  );
+}
+
+export default function EditorToolbar() {
+  return (
+    <ConditionalContents
+      options={[
+        {
+          when: (editor) => editor?.editorType === 'codeblock',
+          contents: () => <ChangeCodeMirrorLanguage />,
+        },
+        {
+          when: (editor) => editor?.editorType === 'sandpack',
+          contents: () => <ShowSandpackInfo />,
+        },
+        {
+          fallback: () => (
+            <>
+              <Button>
+                <SaveIcon strokeWidth={1.5} />
+              </Button>
+              <BoldItalicUnderlineToggles />
+              <CodeToggle />
+              <Separator />
+              <ListsToggle />
+              <Separator />
+              <ConditionalContents
+                options={[
+                  {
+                    when: whenInAdmonition,
+                    contents: () => <ChangeAdmonitionType />,
+                  },
+                  { fallback: () => <BlockTypeSelect /> },
+                ]}
+              />
+              <Separator />
+              <CreateLink />
+              <InsertImage />
+              <Separator />
+              <InsertTable />
+              <InsertThematicBreak />
+              <Separator />
+              <InsertCodeBlock />
+              <ConditionalContents
+                options={[
+                  {
+                    when: (editorInFocus) => !whenInAdmonition(editorInFocus),
+                    contents: () => (
+                      <>
+                        <Separator />
+                        <InsertAdmonition />
+                      </>
+                    ),
+                  },
+                ]}
+              />
+            </>
+          ),
+        },
+      ]}
+    />
+  );
+}
