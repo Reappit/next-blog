2e8aba0237eeade0c3dba9520a8b6d6ed3f0db24
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-11-20 01:33:08 +0100

    save post on server

--- src/repository/post-repository.tsx
+++ src/repository/post-repository.tsx
@@ -1,6 +1,9 @@
 'use server';
-import { type cookies } from 'next/headers';
 import { createServClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import { z } from 'zod';
+import { zfd } from 'zod-form-data';
+import { getCurrentUser } from '@/repository/user-repository';
 
 export async function getPostById(
   cookieStore: ReturnType<typeof cookies>,
@@ -10,7 +13,7 @@ export async function getPostById(
   const { data } = await supabase
     .from('post')
     .select('*, category(*)')
-    .eq('short_id', id)
+    .eq('id', id)
     .limit(1);
 
   return data?.[0];
@@ -24,7 +27,35 @@ export async function getPosts(
   return data ?? [];
 }
 
-export async function savePost(fullStory: string, data: FormData) {
-  console.log(fullStory, data);
-  await Promise.resolve();
+const formSchema = zfd.formData({
+  id: zfd.numeric(),
+  title: zfd.text(z.string().min(20).max(250)),
+  subTitle: zfd.text(z.string().min(0).max(250)),
+  fullStory: zfd.text(z.string().min(0).max(10_000)),
+});
+
+export async function savePost(formData: FormData) {
+  try {
+    const { id, fullStory, subTitle, title } = formSchema.parse(formData);
+    const cookieStore = cookies();
+    const supabase = createServClient(cookieStore);
+    const user = await getCurrentUser(cookieStore);
+    const { data, error } = await supabase
+      .from('post')
+      .upsert({
+        meta_title: '',
+        category: 1,
+        author: user?.id ?? '',
+        full_story: fullStory,
+        subtitle: subTitle,
+        title,
+        ...(id ? { id } : {}),
+      })
+      .select();
+    console.log(error);
+    return Promise.resolve(data);
+  } catch (e) {
+    console.log(e);
+    return Promise.reject('Check input data');
+  }
 }
