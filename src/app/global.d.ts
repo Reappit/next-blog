15865190a51c0735df96031ea033fbd03534ef21
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2023-12-07 15:41:58 +0100

    improve typings

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -3,7 +3,8 @@ import { type Database } from '@/lib/supabase/database.types';
 declare global {
   type DB = Database;
   type tables = DB['public']['Tables'];
-  type PostTable = tables['post']['Row'] & {
-    category: tables['category']['Row'];
+  type PostTable = Omit<tables['post']['Row'], 'category'> & {
+    category: CategoryTable;
   };
+  type CategoryTable = tables['category']['Row'];
 }
