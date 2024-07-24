24e0b1b9640c025431f75632cd464da6846765a7
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-07-24 22:47:49 +0200

    migration to drizzle and sqlite - in progress

--- src/repository/category-repository.tsx
+++ src/repository/category-repository.tsx
@@ -1,5 +1,4 @@
 import { type cookies } from 'next/headers';
-import { createServClient } from '@/lib/supabase/server';
 import { CategoryDto } from '@/repository/dto/category';
 
 export async function getAllCategories(
--- src/repository/user-repository.tsx
+++ src/repository/user-repository.tsx
@@ -1,10 +1,9 @@
-import { createServClient } from '@/lib/supabase/server';
-import { type cookies } from 'next/headers';
-
-export async function getCurrentUser(cookieStore: ReturnType<typeof cookies>) {
-  const supabase = createServClient(cookieStore);
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  return user;
-}
+// import { type cookies } from 'next/headers';
+//
+// export async function getCurrentUser(cookieStore: ReturnType<typeof cookies>) {
+//   const supabase = createServClient(cookieStore);
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
+//   return user;
+// }
