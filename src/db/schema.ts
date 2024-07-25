250595187f50e31c2ef1f2f86825a5460c651048
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-07-26 00:26:20 +0200

    define table schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,35 +1,72 @@
-import { sql } from 'drizzle-orm';
-import { text, sqliteTable, integer } from 'drizzle-orm/sqlite-core';
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
-import { z } from 'zod';
-
-// create table
-// public.post (
-//   category bigint not null,
-//   author uuid not null,
-//   created_at timestamp without time zone not null default now(),
-//   constraint article_pkey primary key (id),
-//   constraint article_meta_title_key unique (meta_title),
-//   constraint post_author_fkey foreign key (author) references "user" (id),
-//   constraint post_category_fkey foreign key (category) references category (id),
-
-
-export const categoryTable = sqliteTable('category', {
-  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-})
+import { relations, sql } from 'drizzle-orm';
+import {
+  text,
+  sqliteTable,
+  integer,
+  primaryKey,
+} from 'drizzle-orm/sqlite-core';
+import { createSelectSchema } from 'drizzle-zod';
 
 export const postTable = sqliteTable('post', {
   id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   title: text('title', { length: 200 }),
-  subtitle: text('subtitle'),
-  meta_title: text('meta_title').unique(),
-  full_story: text('full_story'),
+  subTitle: text('subtitle'),
+  metaTitle: text('meta_title').unique(),
+  fullStory: text('full_story'),
   published: integer('published', { mode: 'boolean' }).notNull().default(false),
-  created_at: text('created_at').notNull().default(sql`(CURRENT_TIMESTAMP)`),
+  createdAt: text('created_at')
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const postRelations = relations(postTable, ({ many }) => ({
+  categories: many(postToCategoryTable),
+}));
+
+export const categoryTable = sqliteTable('category', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text('name').notNull(),
+  metaName: text('meta_name').notNull().unique(),
+  keyword: text('keyword'),
+});
+
+export const categoryRelations = relations(categoryTable, ({ many }) => ({
+  posts: many(postToCategoryTable),
+}));
+
+export const postToCategoryTable = sqliteTable(
+  'post_to_category',
+  {
+    postId: integer('post_id')
+      .notNull()
+      .references(() => postTable.id),
+    categoryId: integer('category_id')
+      .notNull()
+      .references(() => categoryTable.id),
+  },
+  t => ({
+    pk: primaryKey({ columns: [t.postId, t.categoryId] }),
+  })
+);
+
+export const postToCategoryRelations = relations(
+  postToCategoryTable,
+  ({ one }) => ({
+    post: one(postTable, {
+      fields: [postToCategoryTable.postId],
+      references: [postTable.id],
+    }),
+    category: one(categoryTable, {
+      fields: [postToCategoryTable.categoryId],
+      references: [categoryTable.id],
+    }),
+  })
+);
+
+export const tagTable = sqliteTable('tag', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  name: text('name').notNull(),
+  metaName: text('meta_name').notNull().unique(),
 });
 
 export const selectPostSchema = createSelectSchema(postTable);
-
-export const fooTable = sqliteTable('foo', {
-  bar: text('bar').notNull().default('Hey!'),
-});
