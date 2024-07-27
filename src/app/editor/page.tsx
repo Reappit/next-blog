2cb3c6ca12886874152ca26cd74631089d95f27c
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-07-28 00:23:37 +0200

    saving post - in progress

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,10 @@
 import '@mdxeditor/editor/style.css';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
-import { cookies } from 'next/headers';
 import { getAllCategories } from '@/repository/category-repository';
 import { PostDto } from '@/repository/dto/post';
+import { CategoryDto } from '@/repository/dto/category';
+import { getPostById } from '@/repository/post-repository';
 
 const EditorComp = dynamic(() => import('../../components/Editor'), {
   ssr: false,
@@ -16,23 +17,22 @@ interface Props {
 }
 
 export default async function Write({ searchParams: { id } }: Props) {
-  // const cookieStore = cookies();
-  // let categories: CategoryDto[] | null;
-  // let post: PostDto = {} as PostDto;
-  // if (id) {
-  //   const [dbPost, dbCategories] = await Promise.all([
-  //     getPostById(cookieStore, id),
-  //     getAllCategories(cookieStore),
-  //   ]);
-  //   categories = dbCategories;
-  //   post = dbPost ?? PostDto.parse({});
-  // } else {
-  //   categories = await getAllCategories(cookieStore);
-  // }
+  let categories: CategoryDto[] | null;
+  let post: PostDto = {} as PostDto;
+  if (id) {
+    const [dbPost, dbCategories] = await Promise.all([
+      getPostById(+id),
+      getAllCategories(),
+    ]);
+    categories = dbCategories;
+    post = dbPost ?? PostDto.parse({});
+  } else {
+    categories = await getAllCategories();
+  }
 
   return (
     <Suspense fallback={null}>
-      {/*<EditorComp post={post} categories={categories} />*/}
+      <EditorComp post={post} categories={categories} />
     </Suspense>
   );
 }
--- src/repository/post-repository.tsx
+++ src/repository/post-repository.tsx
@@ -4,6 +4,8 @@ import { PostDto } from '@/repository/dto/post';
 import { db } from '@/db';
 import { eq } from 'drizzle-orm';
 import { postTable } from '@/db/schema';
+import { zfd } from 'zod-form-data';
+import { z } from 'zod';
 
 export async function getPosts() {
   const data = await db.query.postTable.findMany({
@@ -47,66 +49,66 @@ export async function getPostById(id: number): Promise<PostDto | undefined> {
 // }
 //
 
-//
-// const formSchema = zfd.formData({
-//   id: zfd.numeric(z.number().optional()),
-//   title: zfd.text(z.string().min(20).max(250)),
-//   subTitle: zfd.text(z.string().min(0).max(250)),
-//   fullStory: zfd.text(z.string().min(0).max(10_000)),
-//   metaTitle: zfd.text(z.string().min(0).max(250)),
-//   category: zfd.numeric(z.number().optional()),
-//   published: zfd.checkbox({ trueValue: 'true' }).optional(),
-// });
-//
-// export async function savePost(formData: FormData) {
-//   try {
-//     const { id, fullStory, subTitle, title, metaTitle, category, published } =
-//       formSchema.parse(formData);
-//     const cookieStore = cookies();
-//     const supabase = createServClient(cookieStore);
-//     const user = await getCurrentUser(cookieStore);
-//     const payload = {
-//       meta_title: metaTitle,
-//       category: category ?? 1,
-//       author: user?.id ?? '',
-//       full_story: fullStory,
-//       subtitle: subTitle,
-//       published: !!published,
-//       title,
-//     };
-//     if (id) {
-//       const { data, error } = await supabase
-//         .from('post')
-//         .update(payload)
-//         .eq('id', id)
-//         .select();
-//       if (error || data?.length === 0) {
-//         throw error || 'Not allow to save';
-//       }
-//       return data;
-//     } else {
-//       const { data, error } = await supabase
-//         .from('post')
-//         .insert(payload)
-//         .select();
-//       if (error || data?.length === 0) {
-//         throw error || 'Not allow to save';
-//       }
-//       return data;
-//     }
-//   } catch (e: unknown) {
-//     if (e instanceof Object && 'message' in e) {
-//       const message = (e as QueryError).message.includes(
-//         'row-level security policy',
-//       )
-//         ? 'Нет доступа'
-//         : e.message;
-//       return Promise.reject(message);
-//     }
-//
-//     if (e instanceof z.ZodError) {
-//       return Promise.reject(e.issues.map((e) => e.message));
-//     }
-//     return Promise.reject(e);
-//   }
-// }
+const formSchema = zfd.formData({
+  id: zfd.numeric(z.number().optional()),
+  title: zfd.text(z.string().min(20).max(250)),
+  subTitle: zfd.text(z.string().min(0).max(250)),
+  fullStory: zfd.text(z.string().min(0).max(10_000)),
+  metaTitle: zfd.text(z.string().min(0).max(250)),
+  category: zfd.numeric(z.number().optional()),
+  published: zfd.checkbox({ trueValue: 'true' }).optional(),
+});
+export async function savePost(formData: FormData) {
+  try {
+    const { id, fullStory, subTitle, title, metaTitle, category, published } =
+      formSchema.parse(formData);
+    // const cookieStore = cookies();
+    // const supabase = createServClient(cookieStore);
+    // const user = await getCurrentUser(cookieStore);
+    const user = {id: ''};
+    const payload = {
+      meta_title: metaTitle,
+      category: category ?? 1,
+      author: user?.id ?? '',
+      full_story: fullStory,
+      subtitle: subTitle,
+      published: !!published,
+      title,
+    };
+
+    if (id) {
+      const { data, error } = await supabase
+        .from('post')
+        .update(payload)
+        .eq('id', id)
+        .select();
+      if (error || data?.length === 0) {
+        throw error || 'Not allow to save';
+      }
+      return data;
+    } else {
+      const { data, error } = await supabase
+        .from('post')
+        .insert(payload)
+        .select();
+      if (error || data?.length === 0) {
+        throw error || 'Not allow to save';
+      }
+      return data;
+    }
+  } catch (e: unknown) {
+    if (e instanceof Object && 'message' in e) {
+      const message = (e as QueryError).message.includes(
+        'row-level security policy',
+      )
+        ? 'Нет доступа'
+        : e.message;
+      return Promise.reject(message);
+    }
+
+    if (e instanceof z.ZodError) {
+      return Promise.reject(e.issues.map((e) => e.message));
+    }
+    return Promise.reject(e);
+  }
+}
