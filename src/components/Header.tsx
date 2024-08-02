d68e4c64a4031977b60458c6a890709d8785659c
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-08-03 00:19:34 +0200

    trigger deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
           <IsAdmin>
             <Button variant="link">
               <Link href="/editor" className="flex items-center">
-                <PenSquare className="mr-1" strokeWidth={0.5} size={17} /> Write
+                <PenSquare className="mr-1" strokeWidth={0.5} size={17} /> Write Story!
               </Link>
             </Button>
           </IsAdmin>
