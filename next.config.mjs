eed6376d303eab0d9b59177e336d4bb4f625fe7c
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-08-05 01:00:05 +0200

    enable ppr

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   ppr: true,
-  // },
   experimental: {
-    serverComponentsExternalPackages: ["@node-rs/argon2"]
-  }
+    ppr: true,
+    serverComponentsExternalPackages: ['@node-rs/argon2'],
+  },
 };
 
 export default nextConfig;
