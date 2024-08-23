8e54ade345119ea4fd1890b9d744a7634c5e67ee
Andrei Karabach <12261380+akarabach@users.noreply.github.com>
2024-08-23 12:24:20 +0200

    use aspect ratio

--- src/app/(post-full-story)/[category]/[slug]/page.tsx
+++ src/app/(post-full-story)/[category]/[slug]/page.tsx
@@ -9,7 +9,9 @@ import { CustomMdx } from '@/components/mdx';
 import PublishedDate from '@/components/PublisedDate';
 import TimeToRead from '@/components/time-to-read';
 import { env } from '@/env';
+import { cn } from '@/lib/utils';
 import postService from '@/services/post-service';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
 
 const postWidth = 680;
 
@@ -26,7 +28,7 @@ export default async function PostPage({
   return (
     <article>
       <div className="flex justify-center">
-        <section className={`w-full max-w-[680px]`}>
+        <section className={cn('w-full', `max-w-[${postWidth}px]`)}>
           <Suspense fallback={<FullPostSkeleton />}>
             <div className="mt-[1.19em]">
               <h1 className="text-4xl font-bold">{post.title}</h1>
@@ -54,15 +56,23 @@ export default async function PostPage({
               </div>
             </IsAdmin>
 
-            <div className="mt-10">
+            <div>
               {post.posterId && (
-                <Image
-                  src={env.NEXT_PUBLIC_IMAGE_BASE_URL + post.posterId}
-                  alt="poster"
-                  sizes={postWidth + 'px'}
-                  width={postWidth}
-                  height={postWidth / 1.5}
-                />
+                <AspectRatio ratio={1.5} className="bg-muted">
+                  <Image
+                    className={cn(
+                      'h-full w-full object-cover'
+                      // 'max-h-[430px] object-cover',
+                      // `w-[${postWidth}px] `
+                    )}
+                    src={env.NEXT_PUBLIC_IMAGE_BASE_URL + post.posterId}
+                    alt={'poster ' + post.metaTitle}
+                    sizes={postWidth + 'px'}
+                    fill
+                    // width={postWidth}
+                    // height={430}
+                  />
+                </AspectRatio>
               )}
             </div>
             <div className="prose mt-10 min-w-full">
